10e9bc043a40d4b5668b0ff72883184ba84df7dc
unknown <jupiter85838@yahoo.com>
2019-09-21 11:21:25 -0400

    multiple inheritance and use of super()

---
+++ multiple-inheritance/app.py
@@ -0,0 +1,48 @@
+class Tokenizer:
+    """Tokenize text"""
+
+    def __init__(self, text):
+        print('Start Tokenizer.__init__()')
+        self.tokens = text.split()
+        print('End Tokenizer.__init__()')
+
+
+class WordCounter(Tokenizer):
+    """Count words in text"""
+
+    def __init__(self, text):
+        print('Start WordCounter.__init__()')
+        super().__init__(text)
+        self.word_count = len(self.tokens)
+        print('End WordCounter.__init__()')
+
+
+class Vocabulary(Tokenizer):
+    """Find unique words in text"""
+
+    def __init__(self, text):
+        print('Start init Vocabulary.__init__()')
+        super().__init__(text)
+        self.vocab = set(self.tokens)
+        print('End init Vocabulary.__init__()')
+
+
+# class TextDescriber(WordCounter, Vocabulary):
+class TextDescriber(Vocabulary, WordCounter):
+    """
+    Describe text with multiple metrics
+    call to Tokenizer.__init__ happened only 
+    once due to use of super() method
+    """
+
+    def __init__(self, text):
+        print('Start init TextDescriber.__init__()')
+        super().__init__(text)
+        print('End init TextDescriber.__init__()')
+
+
+td = TextDescriber('row row row your boat')
+print('--------')
+print(td.tokens)
+print(td.vocab)
+print(td.word_count)
