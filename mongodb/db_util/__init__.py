809df2589725082c5728a97ca48fde5d2b845098
unknown <jupiter85838@yahoo.com>
2020-02-17 15:14:02 -0500

    adding database class

--- mongodb/db_util/__init__.py
+++ mongodb/db_util/__init__.py
@@ -1,3 +1,5 @@
 #from db_util.db_crud import db_start,find_one,post_one,insert_many_records,find_value,delete_one,delete_all_except,update_one_record
 from db_util.db_crud import *
 from db_util.db_initialization import MongoDatabase
+
+#__all__ = []
