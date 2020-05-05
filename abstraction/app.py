bb07cfc3e8e767b651f44283a0b1b449bce0d110
unknown <jupiter85838@yahoo.com>
2020-05-04 21:37:25 -0400

    add str/rep

--- abstraction/app.py
+++ abstraction/app.py
@@ -13,9 +13,36 @@ class Snake(Animal):
     def move(self):
         print(f"snake can crawl !!")
 
+class Person:
+    name = ""
+    age = 0
+
+    def __init__(self, personName, personAge):
+        self.name = personName
+        self.age = personAge
+    # can return any expression
+    def __repr__(self):
+         return '{name:'+self.name+', age:'+str(self.age)+ '}'
+    # Must return string
+    def __str__(self):
+        return 'Person(name='+self.name+', age='+str(self.age)+ ')'
+
 if __name__=="__main__":
     h=Horse()
     h.move()
     s=Snake()
     s.move()
+# ---------------- 
+    p=Person("person",12)
+    # __str__() example
+    print(p)
+    print(p.__str__())
+    s = str(p)
+    print(s)
+    print("\n--------------")
+    # __repr__() example
+    print(p.__repr__())
+    print(f'${type(p.__repr__())}')
+    print(f'{repr(p)}')
+
 
